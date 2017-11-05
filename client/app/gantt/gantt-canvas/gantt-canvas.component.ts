f36ba6f159151005e0e80c1514fec988429d6297
Remi Thebault <remi.thebault@gmail.com>
2017-11-05 21:09:32 +0100

    starting canvas drawing

--- client/app/gantt/gantt-canvas/gantt-canvas.component.ts
+++ client/app/gantt/gantt-canvas/gantt-canvas.component.ts
@@ -28,25 +28,62 @@ export class GanttCanvasComponent implements AfterViewChecked, OnInit {
         this.cycleService.currentCycleObservable.subscribe(
             (cycle: Cycle) => { this.cycle = cycle; }
         );
+        this.checkCanvasSize();
         this.paintCanvas();
     }
 
-    ngAfterViewChecked(): void {
-        let width = this.canvasRef.nativeElement.width;
-        let height = this.canvasRef.nativeElement.height;
-        if (width != this._canvasWidth || height != this._canvasHeight) {
-            this._canvasWidth = width;
-            this._canvasHeight = height;
-            this.ganttTimeMapService.updateCanvasSize(width, height);
+    ngAfterViewChecked() {
+        if (this.checkCanvasSize()) {
+            this.paintCanvas();
         }
     }
 
+    checkCanvasSize(): boolean {
+        let width = this.canvasRef.nativeElement.clientWidth;
+        let height = this.canvasRef.nativeElement.clientHeight;
+
+        if (width != this._canvasWidth || height != this._canvasHeight) {
+            this._canvasWidth = width;
+            this._canvasHeight = height;
+            this.canvasRef.nativeElement.width = width;
+            this.canvasRef.nativeElement.height = height;
+            this.ganttTimeMapService.updateCanvasSize(width, height);
+            return true;
+        }
+        return false;
+    }
+
+    onResize(event) {
+        if (this.checkCanvasSize()) {
+            this.paintCanvas();
+        }
+    }
 
     paintCanvas(): void {
         let ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
-        let width = this.canvasRef.nativeElement.width;
-        let height = this.canvasRef.nativeElement.height;
-        ctx.fillStyle = "aqua";
-        ctx.fillRect(0, 0, width, height);
+        let width = this._canvasWidth;
+        let height = this._canvasHeight;
+
+        const rulerBg = "lightgrey";
+        const rulerLines = "darkgrey";
+        const rulerFg = "black";
+        const rulerHeight = 24;
+
+        ctx.fillStyle = rulerBg;
+        ctx.fillRect(0, 0, width, rulerHeight);
+
+        ctx.strokeStyle = rulerLines;
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(0, rulerHeight+0.5);
+        ctx.lineTo(width, rulerHeight+0.5);
+        ctx.stroke();
+        let grads = this.ganttTimeMapService.grads;
+        for (let g of grads) {
+            ctx.beginPath();
+            ctx.moveTo(g.pos+0.5, 0);
+            ctx.lineTo(g.pos+0.5, height);
+            ctx.stroke();
+        }
     }
 }
--- client/app/gantt/gantt-time-map.service.ts
+++ client/app/gantt/gantt-time-map.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from '@angular/core';
 import { CycleService } from '../services/cycle.service';
 import { Cycle } from "../cycle";
 
+const interCoefs = [ 2, 2.5, 2 ]; // 1, 2, 5, 10, 20, 50, ...
+const minInterGrad = 40;
+
+export class TimeGrad {
+    time: number;
+    pos: number;
+}
+
 @Injectable()
 export class GanttTimeMapService {
 
@@ -10,14 +18,60 @@ export class GanttTimeMapService {
     }
 
     cycle: Cycle;
+    _width: number;
+    _height: number;
+
+    _scale: number;
+    _duration: number;
 
     updateCanvasSize(width: number, height: number) {
+        this._width = width;
+        this._height = height;
+
         if (!this.cycle) {
             this.cycleService.currentCycleObservable.subscribe(
-                (cycle: Cycle) => { this.cycle = cycle; }
+                (cycle: Cycle) => {
+                    this.cycle = cycle;
+                    this.initMap();
+                }
             );
         }
-        console.log("canvas size: %dx%d", width, height);
     }
 
+    initMap() {
+        this._duration = 60;
+        if (this.cycle) this._duration = Math.max(this._duration, this.cycle.cycleTime);
+
+        this._scale = this._width / this._duration;
+    }
+
+    timePos(time: number): number {
+        return time * this._scale;
+    }
+    posTime(pos: number): number {
+        return pos / this._scale;
+    }
+
+    get grads(): TimeGrad[] {
+        let interG = 1;
+        let i = 0;
+        if (this.timePos(interG) < minInterGrad) {
+            interG *= interCoefs[0];
+            if (++i == interCoefs.length) {
+                i = 0;
+            }
+        }
+
+        const numGrads = 1 + this.posTime(this._width) / interG;
+        let grads: TimeGrad[] = new Array(numGrads);
+
+        for (i=0; i<numGrads; ++i) {
+            let t = i * interG;
+            grads[i] = {
+                time: t, pos: Math.round(this.timePos(t))
+            };
+        }
+
+        return grads;
+    }
 }
