b17fa3f7c8497b2bd108d4fff7916c4496ef9aaa
Remi Thebault <remi.thebault@gmail.com>
2017-11-07 22:55:55 +0100

    refactor time map

--- client/app/gantt/gantt-time-map.service.ts
+++ client/app/gantt/gantt-time-map.service.ts
@@ -1,46 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { CycleService } from '../services/cycle.service';
 import { Cycle } from "../cycle";
 
-const interCoefs = [ 2, 2.5, 2 ]; // 1, 2, 5, 10, 20, 50, ...
-const minInterGrad = 40;
+const INTER_COEFS = [ 2, 2.5, 2 ]; // 1, 2, 5, 10, 20, 50, ...
+const MIN_INTER_GRADS = 40;
+
+const DEFAULT_WIDTH = 800;
+const DEFAULT_DURATION = 60;
 
 export class TimeGrad {
     time: number;
     pos: number;
 }
 
-@Injectable()
-export class GanttTimeMapService {
+export class TimeMap {
+    constructor(private _scale: number) {}
 
-    constructor(private cycleService: CycleService)
-    {}
-
-    cycle: Cycle;
-    private _width: number;
-
-    private _scale: number;
-    private _duration: number;
-
-    updateCanvasWidth(width: number) {
-        this._width = width;
-
-        if (!this.cycle) {
-            this.cycleService.currentCycleObservable.subscribe(
-                (cycle: Cycle) => {
-                    this.cycle = cycle;
-                    this.initMap();
-                }
-            );
-        }
-    }
-
-    initMap() {
-        this._duration = 60;
-        if (this.cycle) this._duration = Math.max(this._duration, this.cycle.cycleTime);
-
-        this._scale = this._width / this._duration;
+    get scale(): number {
+        return this._scale;
     }
 
     timePos(time: number): number {
@@ -50,26 +29,101 @@ export class GanttTimeMapService {
         return pos / this._scale;
     }
 
+}
+
+@Injectable()
+export class GanttTimeMapService {
+
+    constructor(private cycleService: CycleService)
+    {
+        // have to initialize with valid values, even though they are replaced
+        // shortly after
+        this._width = DEFAULT_WIDTH;
+        this._duration = DEFAULT_DURATION;
+        this._mapSubject = new BehaviorSubject(
+            new TimeMap(DEFAULT_WIDTH / DEFAULT_DURATION)  // 800 px for 60 time units
+        );
+        this._gradsSubject = new BehaviorSubject([]);
+        this.cycleService.currentCycleObservable.subscribe(
+            (cycle: Cycle) => {
+                this.cycle = cycle;
+                this.initMap();
+            }
+        );
+    }
+
+    cycle: Cycle;
+    private _width: number;
+    private _duration: number;
+    private _widthInitialized: boolean = false;
+
+    private _mapSubject: BehaviorSubject<TimeMap>;
+
+    private _gradsSubject: BehaviorSubject<TimeGrad[]>;
+
+    get map(): TimeMap {
+        return this._mapSubject.value;
+    }
+
+    get mapChange(): Observable<TimeMap> {
+        return this._mapSubject.asObservable();
+    }
+
     get grads(): TimeGrad[] {
+        return this._gradsSubject.value;
+    }
+
+    get gradsChange(): Observable<TimeGrad[]> {
+        return this._gradsSubject.asObservable();
+    }
+
+    updateCanvasWidth(width: number) {
+        if (width !== this._width) {
+            this._width = width;
+            this._gradsSubject.next(this.buildGrads());
+            if (!this._widthInitialized) {
+                this._widthInitialized = true;
+                this.initMap();
+            }
+        }
+    }
+
+    zoom(factor: number): void {
+        this.updateScale(this.map.scale * factor);
+    }
+
+    private initMap() {
+        this._duration = Math.max(DEFAULT_DURATION, this.cycle.cycleTime);
+        this.updateScale(this._width / this._duration);
+    }
+
+    private buildGrads(): TimeGrad[] {
         let interG = 1;
         let i = 0;
-        while (this.timePos(interG) < minInterGrad) {
-            interG *= interCoefs[i];
-            if (++i == interCoefs.length) {
+        while (this.map.timePos(interG) < MIN_INTER_GRADS) {
+            interG *= INTER_COEFS[i];
+            if (++i == INTER_COEFS.length) {
                 i = 0;
             }
         }
 
-        const numGrads = Math.round(1 + this.posTime(this._width) / interG);
+        const numGrads = Math.round(1 + this.map.posTime(this._width) / interG);
         let grads: TimeGrad[] = new Array(numGrads);
 
         for (i=0; i<numGrads; ++i) {
             let t = i * interG;
             grads[i] = {
-                time: t, pos: Math.round(this.timePos(t))
+                time: t, pos: Math.round(this.map.timePos(t))
             };
         }
 
         return grads;
+    }
+
+    private updateScale(scale: number): void {
+        if (scale !== this.map.scale) {
+            this._mapSubject.next(new TimeMap(scale));
+            this._gradsSubject.next(this.buildGrads());
+        }
     }
 }
