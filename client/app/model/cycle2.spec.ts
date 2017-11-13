0a5b0a62cd5026a7fb86afca09ca3add303f27e9
Remi Thebault <remi.thebault@gmail.com>
2017-11-14 00:56:51 +0100

    started new cycle api

---
+++ client/app/model/cycle2.spec.ts
@@ -0,0 +1,44 @@
+
+import * as api from './cycle2';
+
+describe('Cycle simple', () => {
+    let cycle: api.Cycle;
+    beforeEach(() => {
+        cycle = new api.Cycle;
+
+        const t1 = new api.AtomTask("Task 1", 3);
+        const t2 = new api.AtomTask("Task 2", 2);
+        const t3 = new api.AtomTask("Task 3", 5);
+        cycle.pushTask(t1);
+        cycle.pushTask(t2);
+        cycle.pushTask(t3);
+
+        const lc1 = api.Link.createLink(cycle.start, t1.startIn);
+        const l12 = api.Link.createLink(t1.finishOut, t2.startIn);
+        const l23 = api.Link.createLink(t2.finishOut, t3.startIn);
+        const l3c = api.Link.createLink(t3.finishOut, cycle.finish);
+        cycle.pushLink(lc1);
+        cycle.pushLink(l12);
+        cycle.pushLink(l23);
+        cycle.pushLink(l3c);
+    });
+
+    it ("can retrieve basic properties", () => {
+        const st = cycle.startingTasks;
+        const ft = cycle.finishingTasks;
+        expect(st.length).toBe(0);
+        expect(ft.length).toBe(0);
+    });
+
+    it("can plan one cycle", () => {
+        const cp = api.planCycle(cycle, 1);
+        expect(cp.tasks.length).toBe(3);
+        expect(cp.tasks[0].earlyStart).toBe(0);
+        expect(cp.tasks[0].earlyFinish).toBe(3);
+        expect(cp.tasks[1].earlyStart).toBe(3);
+        expect(cp.tasks[1].earlyFinish).toBe(5);
+        expect(cp.tasks[2].earlyStart).toBe(5);
+        expect(cp.tasks[2].earlyFinish).toBe(10);
+        expect(cp.cycleTime).toBe(10);
+    });
+});
---
+++ client/app/model/cycle2.ts
@@ -0,0 +1,353 @@
+export class InDock {
+
+    constructor(private _planner: Planner) {}
+    private _links: Link[]  = [];
+
+    get planner(): Planner {
+        return this._planner;
+    }
+
+    get links(): Link[] {
+        return this._links;
+    }
+
+    pushLink(link: Link): void {
+        this._links.push(link);
+    }
+
+}
+
+export class OutDock {
+
+    constructor(private _planner: Planner) {}
+    private _links: Link[]  = [];
+
+    get planner(): Planner {
+        return this._planner;
+    }
+
+
+    get links(): Link[] {
+        return this._links;
+    }
+    pushLink(link: Link): void {
+        this._links.push(link);
+    }
+}
+
+export class Link {
+    private _from: OutDock;
+    private _to: InDock;
+    private _lag: number;
+
+    get from(): OutDock {
+        return this._from;
+    }
+    get to(): InDock {
+        return this._to;
+    }
+    get lag(): number {
+        return this._lag;
+    }
+    set lag(value: number) {
+        this._lag = value;
+    }
+
+    static createLink(from: OutDock, to: InDock, lag: number=0): Link {
+        const l = new Link;
+        l._from = from;
+        l._to = to;
+        l._lag = lag;
+
+        from.pushLink(l);
+        to.pushLink(l);
+
+        return l;
+    }
+}
+
+
+export abstract class Task implements Planner {
+    private _ind: number;
+    private _name: string;
+    private _startIn    = new InDock(this);
+    private _startOut   = new OutDock(this);
+    private _finishOut  = new OutDock(this);
+
+    constructor(name: string = "") {
+        this._name = name;
+    }
+
+    get ind(): number {
+        return this._ind;
+    }
+    set ind(value: number) {
+        this._ind = value;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get startIn(): InDock {
+        return this._startIn;
+    }
+    get startOut(): OutDock {
+        return this._startOut;
+    }
+    get finishOut(): OutDock {
+        return this._finishOut;
+    }
+
+    abstract forwardPlan(ctx: PlanContext, time: number): number;
+    abstract backwardPlan(ctx: PlanContext, time: number): number;
+}
+
+export class AtomTask extends Task {
+
+    private _duration: number;
+
+    constructor(name: string, duration: number) {
+        super(name);
+        this._duration = duration;
+    }
+
+    get duration(): number {
+        return this._duration;
+    }
+    set duration(value: number) {
+        this._duration = value;
+    }
+
+    forwardPlan(ctx: PlanContext, time: number): number {
+        let tp: TaskPlan = ctx.lookUp(this);
+
+        tp.earlyStart = Math.max(time, tp.earlyStart);
+        tp.earlyFinish = tp.earlyStart + this.duration;
+
+        let res = tp.earlyFinish;
+
+        for(let l of this.startOut.links) {
+            res = Math.max(res, l.to.planner.forwardPlan(ctx, tp.earlyStart+l.lag));
+        }
+        for (let l of this.finishOut.links) {
+            res = Math.max(res, l.to.planner.forwardPlan(ctx, tp.earlyFinish+l.lag));
+        }
+
+        return res;
+    }
+    backwardPlan(ctx: PlanContext, time: number): number {
+        const tp: TaskPlan = ctx.lookUp(this);
+
+        tp.lateFinish = Math.min(time, tp.lateFinish);
+        tp.lateStart = tp.lateFinish - this.duration;
+
+        let res = tp.lateStart;
+
+        for (let l of this.startIn.links) {
+            res = Math.min(res, l.from.planner.backwardPlan(ctx, tp.lateStart-l.lag));
+        }
+
+        return res;
+    }
+}
+
+
+export class Cycle implements Planner {
+    private _name: string;
+    private _start          = new OutDock(this);
+    private _finish         = new InDock(this);
+    private _tasks: Task[]  = [];
+    private _links: Link[]  = [];
+
+    constructor(name: string="") {
+        this._name = name;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get tasks(): Task[] {
+        return this._tasks;
+    }
+
+    get links(): Link[] {
+        return this._links;
+    }
+
+    get start(): OutDock {
+        return this._start;
+    }
+
+    get finish(): InDock {
+        return this._finish;
+    }
+
+    get startingTasks(): Task[] {
+        return this._tasks.filter(t => {
+            return t.startOut.links.length === 0 && t.finishOut.links.length === 0;
+        });
+    }
+
+    get finishingTasks(): Task[] {
+        return this._tasks.filter(t => {
+            return t.startIn.links.length === 0;
+        });
+    }
+
+    pushTask(task: Task): void {
+        this._tasks.push(task);
+    }
+
+    pushLink(link: Link): void {
+        this._links.push(link);
+    }
+
+    forwardPlan(ctx: PlanContext, time: number): number {
+        return time;
+    }
+    backwardPlan(ctx: PlanContext, time: number): number {
+        return time;
+    }
+}
+
+// export class CycleTask extends Task {
+//     private _cycle: Cycle;
+//     private _count: number;
+
+//     constructor(cycle: Cycle, count: number, name?: string) {
+//         super((name && name.length) ? name : cycle.name);
+//         this._cycle = cycle;
+//     }
+
+//     get cycle(): Cycle {
+//         return this._cycle;
+//     }
+
+//     get count(): number {
+//         return this._count;
+//     }
+
+//     set count(value: number) {
+//         this._count = value;
+//     }
+
+//     forwardPlan(inLink: Link, time: number): number {
+//         throw new Error("not implemented");
+//     }
+//     backwardPlan(outLink: Link, time: number): number {
+//         throw new Error("not implemented");
+//     }
+// }
+
+interface Planner {
+    forwardPlan(ctx: PlanContext, time: number): number;
+    backwardPlan(ctx: PlanContext, time: number): number;
+}
+
+export class TaskPlan {
+    private _task: Task;
+    earlyStart: number     = 0;
+    earlyFinish: number    = 0;
+    lateStart: number      = 0;
+    lateFinish: number     = 0;
+
+    visual: any;
+
+    constructor(task: Task) {
+        this._task = task;
+    }
+
+    get isCriticalPath(): boolean {
+        return this.earlyStart === this.lateStart;
+    }
+
+    get slack(): number {
+        return this.lateStart - this.earlyStart;
+    }
+}
+
+
+class PlanContext {
+    constructor(private taskPlans: TaskPlan[]) {
+    }
+
+    lookUp(task: Task) : TaskPlan {
+        return this.taskPlans[task.ind];
+    }
+}
+
+export class CyclePlan {
+    private _cycle: Cycle;
+    private _tasks: TaskPlan[] = [];
+    private _cycleTime: number;
+    private _count: number;
+
+    constructor(cycle: Cycle) {
+        this._cycle = cycle;
+    }
+
+    get tasks(): TaskPlan[] {
+        return this._tasks;
+    }
+
+    get cycleTime(): number {
+        return this._cycleTime;
+    }
+
+    get count(): number {
+        return this._count;
+    }
+
+    plan(count: number): void {
+        let cycle = this._cycle;
+        let tasks: TaskPlan[] = [];
+        cycle.tasks.forEach((t, i) => {
+            t.ind = i;
+        });
+        tasks = cycle.tasks.map(t => {
+            return new TaskPlan(t);
+        });
+
+        const ctx = new PlanContext(tasks);
+
+        let time = 0;
+        for (let t of cycle.startingTasks) {
+            time = Math.max(time, t.forwardPlan(ctx, 0));
+        }
+        for(let l of cycle.start.links) {
+            time = Math.max(time, l.to.planner.forwardPlan(ctx, l.lag));
+        }
+
+        const cycleTime = time;
+        tasks.forEach(t => {
+            t.lateFinish = cycleTime;
+        });
+
+        for (let t of cycle.finishingTasks) {
+            time = Math.min(time, t.backwardPlan(ctx, cycleTime));
+        }
+        for (let l of cycle.finish.links) {
+            time = Math.min(time, l.from.planner.backwardPlan(ctx, cycleTime-l.lag));
+        }
+
+        this._tasks = tasks;
+        this._count = count;
+        this._cycleTime = cycleTime;
+    }
+
+}
+
+export function planCycle(cycle: Cycle, count: number): CyclePlan {
+    const cp = new CyclePlan(cycle);
+    cp.plan(count);
+    return cp;
+}
